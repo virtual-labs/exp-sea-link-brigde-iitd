82e57dd6a949dd1a9503a73bf46c318a48f76665
Virtual Labs IIT Delhi <71967438+vlabsdev-iitd@users.noreply.github.com>
2022-03-19 21:11:21 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -88,24 +88,45 @@
     /////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-    const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+    const myQuestions = [
+        
+        {
+            question: "1.Fourth mode shape involves", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "Symmetrical bending of deck slabs in vertical plane", ///// Write the option 1 inside double quotes
+                b: "Lateral bending of deck", ///// Write the option 2 inside double quotes
+                c: "Axial deformation of deck", ///// Write the option 1 inside double quotes
+                d: "All of the above", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
+        
+        
+        {
+            question: "2. Ninth mode shape frequency is", ///// Write the question inside double quotes
+            answers: {
+                a: "1.276Hz", ///// Write the option 1 inside double quotes
+                b: "1.204 Hz", ///// Write the option 2 inside double quotes
+                c: "4.403Hz", ///// Write the option 1 inside double quotes
+                d: "4.579Hz", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "c" ///// Write the correct option inside double quotes
+        },
+        
+        
+         {
+            question: "3. Ninth mode shape mainly involves following element of the bridge", ///// Write the question inside double quotes
+            answers: {
+                a: "Deck", ///// Write the option 1 inside double quotes
+                b: "Pylon", ///// Write the option 2 inside double quotes
+                c: "Cables", ///// Write the option 1 inside double quotes
+                d: "None of above", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "c" ///// Write the correct option inside double quotes
+        },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
 
+   
 
         
     ];
