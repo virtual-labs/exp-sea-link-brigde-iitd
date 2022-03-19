b94d6095e2606b049e47dc8461077b3774ab83d3
Virtual Labs IIT Delhi <71967438+vlabsdev-iitd@users.noreply.github.com>
2022-03-19 18:25:18 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -88,23 +88,43 @@
      /////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+     const myQuestions = [
+      
+      {
+            question: "1.What type of bridge is represented by Bandra-Worli Sea link bridge?", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "Cantilever bridge",
+                b: "Cable stayed bridge", 
+                c: "Suspension bridge", 
+                d: "None of the above", 
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+      
+       {
+            question: "2.What is the number of spans in Bandra-Worli Sea Link Bridge", ///// Write the question inside double quotes
+            answers: {
+                a: "2",
+                b: "4", 
+                c: "6", 
+                d: "1", 
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
+      
+      {
+            question: "3. Each bridge section (except at the cable - stayed portion) is supported on piers spaced at", ///// Write the question inside double quotes
+            answers: {
+                a: "20m",
+                b: "40m", 
+                c: "10m", 
+                d: "50m", 
+            },
+            correctAnswer: "d" ///// Write the correct option inside double quotes
+        },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
+
+  
 
 
 
